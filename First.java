6983c7dc2903a7d69c59fe75f18e6021420aac5d
prateek747 <prateek.mahesh747@gmail.com>
2016-04-05 21:33:32 +0530

    Added files via upload

---
+++ First.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author prateek
+ *
+ */
+public class First {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
